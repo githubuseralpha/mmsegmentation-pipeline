8bf75d7067adf402ea941bdb5a9fcf1419a6c380
Patryk Swiat <patrykswiat@gmail.com>
2025-01-28 21:45:58 +0000

    better vis

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,9 +1,9 @@
 from mmseg.apis import init_model, inference_model
 from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
 
 config_path = '../configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512.py'
-checkpoint_path = '/workspace/mmsegmentation-pipeline/work_dirs/segformer_mit-b0_8xb2-160k_ade20k-512x512/iter_6000.pth'
+checkpoint_path = '/workspace/mmsegmentation-pipeline/work_dirs/segformer_mit-b0_8xb2-160k_ade20k-512x512/iter_15000.pth'
 
 # init model and load checkpoint
 model = init_model(config_path, checkpoint_path)
@@ -12,9 +12,15 @@ model = init_model(config_path, checkpoint_path)
 img = '/workspace/mmsegmentation-pipeline/scripts/output_images/frame_00010.jpg'
 
 result = inference_model(model, img)
-result_tensor = result.pred_sem_seg.data.cpu().numpy()
+result_np = result.pred_sem_seg.data.cpu().numpy()
 
 # show the results
-print(result_tensor.shape, result_tensor.min(), result_tensor.max())
-plt.imshow(result_tensor.squeeze())
-plt.savefig('plot.png')
+print(result_np.shape, result_np.min(), result_np.max())
+mask = result_np > 0
+# apply mask on original image
+image = np.array(Image.open(img))
+mask = mask[0]
+
+image[mask] = image[mask] * 0.5 + np.array([255, 255, 255]) * 0.5
+
+Image.fromarray(image).save("result.jpg")
