2d6a18e308e696daafa813897081fdde8310f137
Patryk Swiat <patrykswiat@gmail.com>
2025-02-01 20:39:54 +0000

    run and setup

---
+++ run.sh
@@ -0,0 +1,3 @@
+python tools/train.py configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512.py
+python tools/train.py configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512_2.py
+python tools/train.py configs/segformer/segformer_mit-b0_8xb2-160k_ade20k-512x512_3.py
